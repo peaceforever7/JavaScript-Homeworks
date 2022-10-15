cb92eb7130de7ed755a073a24dd172fd7ff9db4f
Azat <110537362+peaceforever7@users.noreply.github.com>
2022-10-15 19:07:28 +0600

    Update app.js

--- HW4_Azat_19-02/Task6/app.js
+++ HW4_Azat_19-02/Task6/app.js
@@ -1,7 +1,7 @@
-var userString = prompt('Введите текст для переворота');
+var userString = prompt("Введите текст для переворота");
 ReverseString(userString)
 function ReverseString(userString) {
-    var reverseString = '';
+    var reverseString = " ";
     for (let i = userString.length-1; i >= 0; i--) {
         reverseString += userString[i];
     }
