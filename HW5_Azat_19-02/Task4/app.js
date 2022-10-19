6c3781defe5267c5d0a6e55124b761983a11dbe6
Azat <110537362+peaceforever7@users.noreply.github.com>
2022-10-19 20:18:33 +0600

    Update app.js

--- HW5_Azat_19-02/Task4/app.js
+++ HW5_Azat_19-02/Task4/app.js
@@ -1,7 +1,6 @@
 const names = ["Evgenii", "Vadim", "Ekaterina", "Sofya", "Maria", "Svetlana", "Alexey"];
 
 console.log(names.reduce(function (maxLength, name) {
-    console.log(name, name.length, maxLength)
     maxLength = maxLength > name.length ? maxLength : name.length
     return maxLength;
 }, 0))
